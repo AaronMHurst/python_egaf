6b8491112ba61ae73bd5a84cafc300bbc05c5dff
Aaron Hurst <amhurst@berkeley.edu>
2023-03-07 07:11:06 -0800

    Added function to return absolute root path of Python package

--- pyEGAF/__init__.py
+++ pyEGAF/__init__.py
@@ -9,3 +9,13 @@ from .file_handler import RIPL, JSONFile, ENSDF
 
 __version__='0.1.0'
 __author__='Aaron M. Hurst'
+
+_ROOT = os.path.abspath(os.path.dirname(__file__))
+def get_data(path):
+    """Function to return absolute path of the data files inside the root of 
+    the Python package."""
+    return os.path.join(_ROOT, path)
+
+print("The JSON-formatted EGAF files are found in:\n {0}".format(get_data('EGAF_JSON')))
+print("The RIPL-formatted EGAF files are found in:\n {0}".format(get_data('EGAF_RIPL')))
+print("The ENSDF-formatted EGAF files are found in:\n {0}".format(get_data('EGAF_ENSDF')))
